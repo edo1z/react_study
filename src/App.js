16665d2a58e29e52a013de290611323778ecefe8
edo1z <edo1z3145@gmail.com>
2017-07-06 09:02:26 +0900

    import semantic-ui-react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Icon } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+	    <h1>Hello React</h1>
+        <Icon name='home' size='large' />
+		<a className="ui button teal">hoge</a>
       </div>
     );
   }
