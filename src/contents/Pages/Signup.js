a031404472888ee607c1d75685a6f67bda63ab0c
edo1z <edo1z3145@gmail.com>
2017-07-09 22:57:31 +0900

    新規登録画面 - APIにPOSTアクセスするテスト

--- src/contents/Pages/Signup.js
+++ src/contents/Pages/Signup.js
@@ -1,11 +1,42 @@
-import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import request from 'superagent'
+import { Container, Form, Button } from 'semantic-ui-react'
 
 class Signup extends Component {
+  state = {num: ''}
+  
+  change = (e) => {
+    this.setState({num:e.target.value})
+  }
+  post = (e) => {
+    request
+      .post('http://localhost:1323/number')
+      .type('form')
+      .send({ number: this.state.num})
+      .set('Accept', 'application/json')
+      .end(function(err, res){
+        if(!err){
+          console.log(res)
+        }
+      })
+    e.preventDefault()
+  }
+  
   render() {
     return (
       <Container>
         <h2>新規登録</h2>
+        
+        <p>APIにPOST送信します</p>
+        
+        <Form onSubmit={(e) => this.post(e)}>
+          <Form.Field>
+            <label>Number</label>
+            <input placeholder='Number' onChange={(e) => this.change(e)} />
+          </Form.Field>
+          <Button type='submit'>Submit</Button>
+        </Form>
+        
       </Container>
     );
   }
